30cdabd2f19d876aea4c3daa9aefcd4bb96b8272
Luca Tringali <tringalinvent@libero.it>
2019-02-26 18:50:56 +0100

    Variable name corrected

--- guercio.bat
+++ guercio.bat
@@ -1,7 +1,7 @@
 netsh advfirewall set allprofiles state off
 cd %~dp0
 cd UltraVNC
-find /c "path" ultravnc.ini  > nul || ( for /r %%i in (*.dll) do regsvr32 /s %%i)
+find /c "path" ultravnc.ini  > nul || ( for /r %i in (*.dll) do regsvr32 /s %i)
 find /c "path" ultravnc.ini  > nul || ( echo "path=%~dp0\UltraVNC" >> ultravnc.ini)
 cd ..
 start UltraVNC\winvnc.exe -run
